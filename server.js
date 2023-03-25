7cfb4f9a704ab8ec7e3a8648a7a7cb61ed0822d4
thandyn <nguyenphuthan@gmail.com>
2023-03-24 17:50:45 -0700

    added delete method

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const path = require("path");
-const notes = require("./db/db.json");
+const fs = require("fs");
+const uniqid = require("uniqid");
+// let notes = require("./db/db.json");
+let newNotes = {
+  title,
+  text,
+  id: uniqid(),
+};
 
 const app = express();
 const PORT = 3001;
@@ -19,17 +26,79 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.json(JSON.parse(data));
+  });
 });
 
 app.post("/api/notes", (req, res) => {
-  let newNote = {
-    title: req.body.title,
-    text: req.body.text,
-  };
+  const { title, text } = req.body;
+  if (title && text) {
+    // let newNotes = {
+    //   title,
+    //   text,
+    //   id: uniqid(),
+    // };
+    const notesData = JSON.stringify(newNotes);
 
-  notes.push(newNote);
-  res.json(200);
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+
+      const note = JSON.parse(data);
+      note.push(newNotes);
+
+      fs.writeFile(`./db/db.json`, JSON.stringify(note), (err) =>
+        err
+          ? console.error(err)
+          : console.log(
+              `Notes for ${newNotes.title} has been written to JSON file`
+            )
+      );
+      const response = {
+        status: "success",
+        body: newNotes,
+      };
+      console.log(response);
+      res.status(200).json(response);
+    });
+  } else {
+    res.json(`Please include title`);
+  }
+  console.log(req.body);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+
+    let notes = JSON.parse(data);
+
+    notes = notes.filter((note) => note.id !== req.params.id);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Failed to delete note" });
+      }
+      console.log(
+        `Note with ID ${req.params.id} has been deleted from JSON file`
+      );
+
+      const response = {
+        status: "success",
+        message: `Note with ID ${req.params.id} has been deleted`,
+      };
+      console.log(response);
+      res.status(200).json(response);
+    });
+  });
 });
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
